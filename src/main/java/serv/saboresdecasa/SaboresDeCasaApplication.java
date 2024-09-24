6f5302ac3b828a9bc4b61ac28c7bd6f068fc1a62
DavidClase <dperezfernandez@safareyes.es>
2024-09-24 16:50:27 +0200

    🎉 Launch project

---
+++ src/main/java/serv/saboresdecasa/SaboresDeCasaApplication.java
@@ -0,0 +1,13 @@
+package serv.saboresdecasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaboresDeCasaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaboresDeCasaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/serv/saboresdecasa/SaboresDeCasaApplicationTests.java
@@ -0,0 +1,13 @@
+package serv.saboresdecasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaboresDeCasaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
