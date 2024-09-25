3110fe249ed20cd482ea98b7122d6c56435a8120
DavidClase <dperezfernandez@safareyes.es>
2024-09-25 20:09:21 +0200

    ✅ Add a test

--- src/test/java/serv/saboresdecasa/SaboresDeCasaApplicationTests.java
+++ src/test/java/serv/saboresdecasa/SaboresDeCasaApplicationTests.java
@@ -1,13 +1,25 @@
 package serv.saboresdecasa;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import serv.saboresdecasa.model.Bebida;
+import serv.saboresdecasa.repository.BebidaRepository;
+
+import java.util.List;
 
 @SpringBootTest
 class SaboresDeCasaApplicationTests {
 
+    @Autowired
+    private BebidaRepository bebidaRepository;
+
     @Test
     void contextLoads() {
+        List<Bebida> bebidas = bebidaRepository.findAll();
+        for (Bebida bebida : bebidas) {
+            System.out.println(bebida.getNombre());
+        }
     }
 
 }
