30a0dd81a10bfc42a36096dc1e8edb383392a38b
DavidClase <dperezfernandez@safareyes.es>
2024-10-09 20:57:13 +0200

    ✨ Created controller for orders

---
+++ src/main/java/serv/saboresdecasa/controller/PedidoController.java
@@ -0,0 +1,60 @@
+package serv.saboresdecasa.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import serv.saboresdecasa.dto.PedidoDTO;
+import serv.saboresdecasa.dto.PlatoDTO;
+import serv.saboresdecasa.dto.PlatoPedidoDTO;
+import serv.saboresdecasa.model.Bebida;
+import serv.saboresdecasa.model.Plato;
+import serv.saboresdecasa.model.PlatoPedido;
+import serv.saboresdecasa.service.PedidoService;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/pedido")
+public class PedidoController {
+    private PedidoService pedidoService;
+
+    @GetMapping
+    public List<PedidoDTO> getAll() {
+        return pedidoService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public PedidoDTO getById(@PathVariable Integer id) {
+        return pedidoService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        pedidoService.delete(id);
+    }
+
+    @PostMapping
+    public PedidoDTO save(@RequestBody PedidoDTO pedido) {
+        return pedidoService.save(pedido);
+    }
+
+    @GetMapping("/{id}/platos")
+    public List<PlatoDTO> getPlatos(@PathVariable Integer id) {
+        return pedidoService.getPlatos(id);
+    }
+
+    @GetMapping("/{id}/bebidas")
+    public List<Bebida> getBebidas(@PathVariable Integer id) {
+        return pedidoService.getBebidas(id);
+    }
+
+    @PostMapping("/{id}/plato")
+    public PlatoPedidoDTO addPlato(@PathVariable Integer id, @RequestBody PlatoPedidoDTO plato) {
+        return pedidoService.addPlato(id, plato);
+    }
+
+    @GetMapping("/{id}/price")
+    public Double getPrice(@PathVariable Integer id) {
+        return 0.0;
+    }
+}
