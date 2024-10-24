6b0144283db1e11e1568f01fff9143695465ae31
DavidClase <dperezfernandez@safareyes.es>
2024-10-24 15:14:01 +0200

    🔥 Removed fields from PedidoTotalPriceDTO

--- src/main/java/serv/saboresdecasa/dto/PedidoTotalPriceDTO.java
+++ src/main/java/serv/saboresdecasa/dto/PedidoTotalPriceDTO.java
@@ -1,21 +1,19 @@
 package serv.saboresdecasa.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class PedidoTotalPriceDTO {
     Integer id;
     Integer idCliente;
-    Short numMesa;
-    String fecha;
     Integer idPromocion;
     Double totalPrice;
 
     public PedidoTotalPriceDTO(PedidoDTO pedidoDTO) {
         this.id = pedidoDTO.getId();
         this.idCliente = pedidoDTO.getIdCliente();
-        this.numMesa = pedidoDTO.getNumMesa();
-        this.fecha = pedidoDTO.getFecha();
         this.idPromocion = pedidoDTO.getIdPromocion();
         this.totalPrice = 0.0;
     }
